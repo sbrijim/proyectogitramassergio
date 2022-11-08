f2333d79fee169d8b137e8c7d06294d138c6bdcd
usuario <usuario@T4-16>
2022-11-08 21:33:01 +0100

    Commit inicial
    una clase main que...

---
+++ EJEMPLO/src/ej1.java
@@ -0,0 +1,9 @@
+public class ej1 {
+
+	public static void main(String[] args) {
+		potenciaDos(6+5+7+8);
+	}
+		public static void potenciaDos(int numero) { 
+		System.out.println(numero * numero);
+	}
+}
